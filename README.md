7e59878f989bf6776534a6bd807575261c462dc1
t4ichi <taichinoheya020316@icloud.com>
2024-09-26 21:26:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# vscoderc
+```
+sh ./install_vscode_settings_for_macos.sh
+````
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+cp -f ./src/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+cp -f ./src/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+echo "installed!" 
